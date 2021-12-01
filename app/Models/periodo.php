d79389b9931516e7cdbf2d07d48e70c6c2abeb94
alvaradomonge <alvarado.monge@hotmail.com>
2021-12-01 14:29:25 -0600

    Update periodo.php
    Se agrega la funcion materias

--- app/Models/periodo.php
+++ app/Models/periodo.php
@@ -15,4 +15,8 @@ class periodo extends Model
     public function anio_lectivo(){
         return $this->belongsToMany('App\Models\anio_lectivo','rel_anio_periodo','id_periodo','id_anio');
     }
+
+    public function materias(){
+        return $this->belongsToMany(materia::class, 'rel_periodo_materia','id_periodo','id_materia');
+    }
 }
