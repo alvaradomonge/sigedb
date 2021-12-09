0059c48c3a385b3ae17d43208a4e24aa06b06d34
alvaradomonge <alvarado.monge@hotmail.com>
2021-12-09 12:51:31 -0600

    Reutilizar request de estudiantes para crear usuarios de user

--- app/Http/Requests/SaveUserRequest.php
+++ app/Http/Requests/SaveUserRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class SaveEstudianteRequest extends FormRequest
+class SaveUser extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,21 +24,26 @@ class SaveEstudianteRequest extends FormRequest
     public function rules()
     {
         return [
-            'nombre'=> 'required',
-            'apellido1'=> 'required',
-            'apellido2'=> 'required',
+            'name'=> 'required|max:20',
+            'apellido1'=> 'required|max:20',
+            'apellido2'=> 'required|max:20',
             'email'=> 'required|email',
             'cedula'=>'nullable',
             'telefono'=>'nullable',
             'direccion'=>'nullable'
+            'id_estado'=>'required',
+            'id_rol_usuario'=>'required'
         ];
     }
     public function messages()
     {
         return [
             'nombre.required'=>'Por favor ingrese el nombre',
+            'nombre.max'=>'El nombre no debe exceder los 20 caracteres',
             'apellido1.required'=>'Por favor ingrese el primer apellido',
+            'apellido1.max'=>'El apellido no debe exceder los 20 caracteres',
             'apellido2.required'=>'Por favor ingrese el segundo apellido',
+            'apellido2.max'=>'El apellido no debe exceder los 20 caracteres',
             'email.email'=>'Ingrese un correo electrónico válido',
             'email.required'=>'El correo electrónico es obligatorio',
 
