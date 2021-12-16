8a0be948f80ca500dff992bcf3667dc1adc9ad22
alvaradomonge <alvarado.monge@hotmail.com>
2021-12-16 08:32:57 -0600

    footer dinámico funcionando

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -13,13 +13,13 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 	<style>
 	
-	body{
+	/*body{
 		position: relative;
 		padding-bottom: 3em;
-		min-height: 130vh;
-	}
+		min-height: 100vh;
+	}*/
 	main{
-		min-height: 130vh;
+		min-height: 90vh;
 	}
 
 	</style>
