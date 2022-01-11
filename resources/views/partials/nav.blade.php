84ec05def992856291db3c8cf7fe198666962a37
alvaradomonge <alvarado.monge@hotmail.com>
2022-01-11 16:24:33 -0600

    Update nav.blade.php
    Actualizaciones menores en el navbar para mejorar experiencia

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-light navbar-expand-lg bg-white shadow-sm">
-	<div class="container">
+	<div class="container-fluid">
 		<a class="navbar-brand" href="{{route('inicio')}}">{{config('app.name')}}</a>
 		<button class="navbar-toggler" 
 			type="button" 
@@ -12,57 +12,51 @@
 	    </button>
 	    
 	    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-		    <div class="">
-		    	<div class="">
-					<ul class="nav nav-pills">
-						<li class="nav-item ">
-							<a class="nav-link {{setActive('inicio')}}" 
-							href="{{route('inicio')}}">Inicio</a>
-						</li>
-						@auth
-							@if (auth()->user()->id_rol_usuario == '1')
-								@include('partials/navAdmin')
-							@elseif(auth()->user()->id_rol_usuario == '2')
-								@include('partials/navDocente')
-							@elseif(auth()->user()->id_rol_usuario == '3')
-								@include('partials/navEstudiante')
-							@elseif(auth()->user()->id_rol_usuario == '4')
-								@include('partials/navCoordinador')
-							@endif
-						@endauth
-						@guest
-							<li class="nav-item">
-								<a class="nav-link {{setActive('informenotas')}}" href="{{route('informenotas')}}"
-							>Contacto</a>
-							</li>
-							<li class="nav-item"><a class="nav-link {{setActive('login')}}" href="{{route('login')}}">Login</a></li>
-						@else
-							<li class="nav-item">
-								<a class="nav-link" href="#" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-									Cerrar Sesión
-								</a>
-							</li>
-						@endguest
-						
-					</ul>
-				</div>
+			<ul class="nav nav-pills">
+				<li class="nav-item ">
+					<a class="nav-link {{setActive('inicio')}}" 
+					href="{{route('inicio')}}">Inicio</a>
+				</li>
 				@auth
-					<div class="">
-						<small>{{auth()->user()->name}}
-						@if(auth()->user()->id_rol_usuario == '1')
-							<small>(Administrador)</small>
-						@elseif(auth()->user()->id_rol_usuario == '2')
-							<small>(Docente)</small>
-						@elseif(auth()->user()->id_rol_usuario == '3')
-							<small>(Estudiante)</small>
-						@elseif(auth()->user()->id_rol_usuario == '4')
-							<small >(Coordinador)</small>
-						@endif
-						</small>
-					</div>
+					@if (auth()->user()->id_rol_usuario == '1')
+						@include('partials/navAdmin')
+					@elseif(auth()->user()->id_rol_usuario == '2')
+						@include('partials/navDocente')
+					@elseif(auth()->user()->id_rol_usuario == '3')
+						@include('partials/navEstudiante')
+					@elseif(auth()->user()->id_rol_usuario == '4')
+						@include('partials/navCoordinador')
+					@endif
 				@endauth
+				@guest
+					<li class="nav-item">
+						<a class="nav-link {{setActive('informenotas')}}" href="{{route('informenotas')}}"
+					>Contacto</a>
+					</li>
+					<li class="nav-item"><a class="nav-link {{setActive('login')}}" href="{{route('login')}}">Login</a></li>
+				@else
+					<li class="nav-item">
+						<a class="nav-link" href="#" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+							Cerrar Sesión
+						</a>
+					</li>
+				@endguest
+				
+			</ul>
+		@auth
+			<div class="d-flex me-2 btn btn-light">
+				{{auth()->user()->name}}
+				@if(auth()->user()->id_rol_usuario == '1')
+					<small >(Administrador)</small>
+				@elseif(auth()->user()->id_rol_usuario == '2')
+					<small>(Docente)</small>
+				@elseif(auth()->user()->id_rol_usuario == '3')
+					<small>(Estudiante)</small>
+				@elseif(auth()->user()->id_rol_usuario == '4')
+					<small >(Coordinador)</small>
+				@endif	
 			</div>
-		</div>
+		@endauth
 	</div>
 </nav>
 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
